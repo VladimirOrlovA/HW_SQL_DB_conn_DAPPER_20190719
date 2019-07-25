e041c45b3f1bc176fc753964635dea5049924560
Vladimir Orlov <vladimir.orlov.a@mail.ru>
2019-07-25 11:55:08 +0600

    Add project files.

---
+++ HW_SQL_DB_conn_DAPPER_20190719/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_SQL_DB_conn_DAPPER_20190719
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
